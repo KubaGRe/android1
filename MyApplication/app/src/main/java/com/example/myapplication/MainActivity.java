88c5d4410e1385157e9517a6656266c3527d080e
jgrela <jgrela@d17-327e-pc-06.ipa.ii.agh.edu.pl>
2023-04-22 12:01:19 +0200

    myapp

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,11 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void increase(){
+
     }
+
+    public void decrease(){
+
+    }
+
+    public void zero(){
+
+    }
+
+
 }
