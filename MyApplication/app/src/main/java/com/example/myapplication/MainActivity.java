1c60242e2b4316c1ca224c80624e0ed2153fe139
jgrela <jgrela@d17-327e-pc-06.ipa.ii.agh.edu.pl>
2023-04-22 12:14:53 +0200

    Change task

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int counter = 0;
     public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
 
     @Override
@@ -17,16 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void increase(){
-
+    public void increaseCounter(View view){
+        counter ++;
+        TextView textView = (TextView) findViewById(R.id.counter_view);
+        textView.setText((String.valueOf(counter)));
     }
 
-    public void decrease(){
-
+    public void decreaseCounter(View view){
+        counter --;
+        TextView textView = (TextView) findViewById(R.id.counter_view);
+        textView.setText((String.valueOf(counter)));
     }
 
-    public void zero(){
-
+    public void zeroCounter(View view){
+        counter =0;
+        TextView textView = (TextView) findViewById(R.id.counter_view);
+        textView.setText((String.valueOf(counter)));
     }
 
 
